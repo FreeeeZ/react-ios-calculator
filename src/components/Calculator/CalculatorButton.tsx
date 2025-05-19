555458475c6eb1fed4d3949f12f9bbd9e395ea7b
Vladislav Shell <shell-vladislav@mail.ru>
2025-05-20 00:35:21 +0700

    Set up vercel deploy

--- src/components/Calculator/CalculatorButton.tsx
+++ src/components/Calculator/CalculatorButton.tsx
@@ -4,29 +4,27 @@ import { type CalculatorActions } from '@components/Calculator/Calculator';
 
 type CalculatorButtonProps = {
   label: string;
+  action: CalculatorActions;
+  /* eslint-disable */
   onClick: (action: CalculatorActions, value?: string) => void;
+  /* eslint-enable */
   value?: string;
-  action?: string;
   color?: 'gray' | 'orange' | 'dark';
   wide?: boolean;
 };
 
 export const CalculatorButton: React.FC<CalculatorButtonProps> = ({
   label,
+  action,
   onClick,
   value = label,
-  action = 'addDigit',
   color = 'gray',
   wide = false,
 }) => {
-  const handleClick = () => {
-    onClick(action, value);
-  };
-
   return (
     <button
       className={`${styles.button} ${styles[color]} ${wide ? styles.wide : ''}`}
-      onClick={handleClick}
+      onClick={() => onClick(action, value)}
       aria-label={label}
     >
       {label}
